2da95d8b685a238c183b537777f73f0f4598634e
sanskar <sanskar362002@gmail.com>
2024-01-27 01:11:45 +0530

    Frontend url added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost:5173"
+    "https://kmc-esports-frontend.vercel.app"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
